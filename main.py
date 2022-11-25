48cf72b81d43902704515150e7b6b72964122990
Cyprien Bouton <cyprien.bouton@mines-paristech.fr>
2022-11-25 19:42:41 +0100

    [FIX] the split in main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import pandas as pd
 import re
 
-data = pd.read_csv('dset/valeursfoncieres-2022-s1.csv', on_bad_lines='skip')
+data = pd.read_csv('dset/valeursfoncieres-2022-s1.txt', on_bad_lines='skip')
 
 def split_values(x):
-    return re.split("\|",str(x))
+    return re.split("\|",str(x.values[0]))
 
-columns = split_values(data.columns[0])
-
-def transform_row(df):
-    df.iloc[:,0] = df.iloc[:,0].apply(split_values)
-    return df
-
-data = transform_row(data)
-for idx,column in enumerate(columns):
-    data[column] = data.apply(lambda x:x)#[idx])
+data = data.apply(split_values,axis=1)
 
 if __name__ == "__main__":
-    print(data)
+    print(data.iloc[0])
 
