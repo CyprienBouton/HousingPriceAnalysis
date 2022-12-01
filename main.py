5b670f82e8c13d3a33c2ee523db357d3e6f17c97
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-01 23:25:21 +0100

    [UP] main.py

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-import pandas as pd
-import re
-import streamlit as st
-from extract_transform.load_data import load_data
 import pickle
+import pandas as pd
+import streamlit as st
+from extract_transform import clean_data, remove_outlier, keep_flat_house, add_delta_days
 
 data = pd.DataFrame()
 for year in range(2017,2023):
     data_year = load_data(year)
     data = pd.concat([data,data_year])
-    print(data.shape, data_year.shape)
+
+data = clean_data.clean_data(data)
+
+Bounds = {
+    "Built surface min": 9,
+    "Built surface max": 1000,
+    "Ground surface max": 100000,
+    "Transaction cost min": 1000,
+    "Transaction cost max": 10000000}
+Nsigma = 1.5 # any data superior to 1.5 times its standard deviation will be removed
+data = remove_outlier.remove_outlier(data, Nsigma, Bounds)
+data = keep_flat_house.keep_flat_house(data)
+data = add_delta_days.add_delta_days(data)
+print(data.head(5))
 
 # we save the data in a pickle file
-file = open('data.pkl', 'wb')
+file = open('dset/data.pkl', 'wb')
 pickle.dump(data, file)
