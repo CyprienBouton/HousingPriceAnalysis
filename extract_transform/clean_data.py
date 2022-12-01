3f13deb8bb1856abb09dd9e54d1c575fd51769c0
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-01 21:54:49 +0100

    [ADD] clean_data

---
+++ extract_transform/clean_data.py
@@ -0,0 +1,9 @@
+def clean_data(data):
+    """
+    Clean the pandas dataframe as follow:
+        2.1. Replace by type 'Vente' (sell) all the naming involving vente (sell) activity.
+        2.2. Remove all transactions not 'Vente' (sell)  (eg 'echange', 'expropriation',...)
+    """
+    data['Transaction type'] = data['Transaction type'].replace(to_replace="^Vente", value="Vente", regex = True)
+    data = data[data['Transaction type'] == 'Vente']
+    return data
