4114b4d440d387accbb4c1ad4fd049ee39f54dff
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-03 11:21:10 +0100

    [UP] extract_transform

--- extract_transform/add_delta_days.py
+++ extract_transform/add_delta_days.py
@@ -1,7 +1,12 @@
 import pandas as pd
 def add_delta_days(data):
+    """
+        1. Add a column Delta days: Date - Date of the first transaction
+        2. Remove column Transaction date
+    """
     #Divide by oneday to get ride of type timedelta
     oneday = pd.Timedelta(days=1)
     data["Transaction date"]= pd.to_datetime(data["Transaction date"]) #convert str to pd.datetime
     data['Delta days'] = (data['Transaction date'] - data['Transaction date'].min())/oneday
+    data = data.drop('Transaction date', axis=1)
     return data
--- extract_transform/clean_data.py
+++ extract_transform/clean_data.py
@@ -1,12 +1,14 @@
 def clean_data(data):
     """
     Clean the pandas dataframe as follow:
-        2.1. Replace by type 'Vente' (sell) all the naming involving vente (sell) activity.
-        2.2. Remove all transactions not 'Vente' (sell)  (eg 'echange', 'expropriation',...)
-        2.3. Drop nan values
+        1. Replace by type 'Vente' (sell) all the naming involving vente (sell) activity.
+        2. Remove all transactions not 'Vente' (sell)  (eg 'echange', 'expropriation',...)
+        3. Remove the column Transaction type
+        4. Drop nan values
     """
     data['Transaction type'] = data['Transaction type'].replace(to_replace="^Vente", value="Vente", regex = True)
     data = data[data['Transaction type'] == 'Vente']
     data = data.dropna()
+    data = data.drop("Transaction type", axis=1)
     return data
     
