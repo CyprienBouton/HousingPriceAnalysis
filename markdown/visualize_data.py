3f4dfcea679ff9644ea873940d6c65dfe95643dc
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-04 20:23:36 +0100

    [UP]visualize using plotly and streamlit

--- markdown/visualize_data.py
+++ markdown/visualize_data.py
@@ -1,9 +1,15 @@
 import pickle
+import plotly.express as px
 import streamlit as st
+import matplotlib.pyplot as plt
 
 file = open("dset/data.pkl","rb")
 data =pickle.load(file)
+
 data['latitude']=data['latitude'].astype(int)
 data['longitude']=data['longitude'].astype(int) 
-# fig =  px.choropleth(data, lat="Latitude", lon="Longitude")
-st.map(data, zoom=None)
+fig =  px.scatter_geo(data, lat="latitude", lon="longitude",
+ scope='europe',center={'lat':48.9,'lon':2.3})
+# fig.show()
+st.plotly_chart(fig)
+# st.map(data)
