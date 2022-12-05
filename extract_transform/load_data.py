969eb0b387de85f0ca1b4410c7df469854920b0f
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-05 17:33:49 +0100

    [ADD] remove outlier for Cost per m2

--- extract_transform/load_data.py
+++ extract_transform/load_data.py
@@ -16,19 +16,19 @@ def load_data(year):
       'lot5_surface_carrez', 'nombre_lots', 'code_type_local', 'Type of property',
       'Built surface', 'Number of rooms',
       'code_nature_culture', 'nature_culture', 'code_nature_culture_speciale',
-      'nature_culture_speciale', 'Ground surface', 'longitude', 'latitude']
+      'nature_culture_speciale', 'Ground surface', 'Longitude', 'Latitude']
 
    # keep the most relevant cols
    col2use = ['Transaction cost', 'Transaction date', 'Transaction type', 'Type of property',
-    'Built surface', 'Number of rooms', 'Ground surface', 'longitude',
-      'latitude', 'Town']
+    'Built surface', 'Number of rooms', 'Ground surface', 'Longitude',
+      'Latitude', 'Town']
    typecols = {'Transaction cost':float,
       'Transaction type':str,                
       'Built surface':float,
       'Number of rooms':float,
       'Ground surface':float,
-      'longitude':float,
-      'latitude':float,
+      'Longitude':float,
+      'Latitude':float,
       'Town':str}
    return pd.read_csv('dset/full_'+str(year)+".csv.gz", header=0,
      names=col_names, usecols=col2use, dtype=typecols, compression='gzip')
--- extract_transform/remove_outlier.py
+++ extract_transform/remove_outlier.py
@@ -3,22 +3,19 @@ from scipy import stats
 
 def remove_outlier(data, Nsigma, Bounds):
     """
-    1. Remove the unlikely transaction based on human judgment: Bounds records
+    1. Create a column 'Cost per m2'
+    2. Remove the unlikely transaction based on human judgment: Bounds records
     2. Remove the outlier transactions (to avoid misleading analysis results)
     Bounds is a dictonary with the following keys: 
-        Built surface min
-        Built surface max
-        Ground surface max
-        Transaction cost min
-        Transaction cost max 
-    """    
-    
+   """    
+    data['Cost per m2']=data['Transaction cost']/data['Built surface']
     data = data[ (data['Built surface'] > Bounds['Built surface min']) &
          (data['Built surface'] < Bounds['Built surface max']) &
-         (data['Ground surface'] < Bounds['Ground surface max']) &
          (data['Transaction cost'] > Bounds['Transaction cost min']) &
-         (data['Transaction cost'] < Bounds['Transaction cost max'])]
+         (data['Transaction cost'] < Bounds['Transaction cost max']) &
+         (data['Cost per m2'] > Bounds['Cost per m2 min']) &
+         (data['Cost per m2'] < Bounds['Cost per m2 max'])]
     
-    datared = data[['Built surface', 'Ground surface', 'Transaction cost']]
+    datared = data[['Built surface', 'Transaction cost', 'Cost per m2']]
     data = data[(np.abs(stats.zscore(datared)) < Nsigma).all(axis=1)]
     return data
