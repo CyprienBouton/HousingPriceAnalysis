4aaff22aea04fae912ae685b07c93937dc956df9
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-05 11:21:25 +0100

    [UP] src/model.py

--- src/model.py
+++ src/model.py
@@ -2,10 +2,12 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import  mean_squared_error
 import pickle
+import matplotlib.pyplot as plt
 import numpy as np
 
-def relative_error(true, pred):
-    return (100*np.divide(np.abs(true-pred),true)).mean()
+
+def MAPE(true, pred):
+    return (100*np.abs(true-pred)/true).mean()
 
 file = open("dset/data.pkl","rb")
 data = pickle.load(file)
@@ -13,7 +15,9 @@ data = pickle.load(file)
 x_train, x_test, y_train, y_test = train_test_split(data.iloc[:,1:], 
 data.iloc[:,:1].values.ravel(), test_size=.2) # split train_set and test_set
 
-model = RandomForestRegressor()
+
+model = RandomForestRegressor(n_estimators=5)
 model.fit(x_train, y_train)
-preds = model.predict(x_test)
-print(relative_error(y_test,preds), "/nmse", mean_squared_error(y_test, preds))
+for i in range(50):
+    preds = model.predict([x_test.iloc[i]])
+    print(MAPE([y_test[i]],preds), y_test[i], preds)
