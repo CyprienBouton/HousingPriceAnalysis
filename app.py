5ae972987ad7eb86adc065380bbb0a519226e106
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-06 17:54:11 +0100

    [ADD] app.py main file of the streamlit app

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from markdown import main_page, page2, page3, page4
+
+page_names_to_funcs = {
+    "General Description": main_page.main_page,
+    "Introduction": page2.page2,
+    "Data vizualisation": page3.page3,
+    "Flat Price prediction": page4.page4
+}
+
+selected_page = st.sidebar.selectbox("Go to page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
