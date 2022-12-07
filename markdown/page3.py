527d807c6f41f76a82cda6bb7031d17d54fdfcac
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-07 16:02:11 +0100

    [UP] page 3

--- markdown/page3.py
+++ markdown/page3.py
@@ -1,9 +1,8 @@
 import streamlit as st
-from .visualize_data import cost_per_m2, price_per_surface
+from visualize import cost_per_m2, price_per_surface
 def page3():
     st.markdown("<h1>Data visualization</h1>",
      unsafe_allow_html=True)
     st.markdown("See how the following paramters effect on the price:")
-    st.pyplot(price_per_surface())
-    st.pyplot(cost_per_m2())
-    
+    st.pyplot(price_per_surface.price_per_surface())
+    st.pyplot(cost_per_m2.cost_per_m2())
