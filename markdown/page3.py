82de22916313b7728828e0acbef41315b614149f
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-07 15:04:59 +0100

    [UP] markdown vizualisation

--- markdown/page3.py
+++ markdown/page3.py
@@ -1,7 +1,9 @@
 import streamlit as st
-from .visualize_data import cost_per_m2
+from .visualize_data import cost_per_m2, price_per_surface
 def page3():
     st.markdown("<h1>Data visualization</h1>",
      unsafe_allow_html=True)
+    st.markdown("See how the following paramters effect on the price:")
+    st.pyplot(price_per_surface())
     st.pyplot(cost_per_m2())
     
--- markdown/visualize_data.py
+++ markdown/visualize_data.py
@@ -1,5 +1,6 @@
 import pickle
 import matplotlib.pyplot as plt
+import numpy as np
 
 file = open("dset/data.pkl","rb")
 data =pickle.load(file)
@@ -9,12 +10,31 @@ def cost_per_m2(nb_points=50000):
     df = data[:nb_points]
     fig, ax = plt.subplots(figsize=(3.,2.3))
     points = ax.scatter(df['Longitude'], df['Latitude'], c=df['Cost per m2'], s=5, cmap="plasma")
-    ax.title.set_text("Cost per square meter according to location")
-    ax.set_xlabel("Longitude")
-    ax.set_ylabel("Latitude")
-    fig.colorbar(points)
+    ax.title.set_text("Paris real estate maps")
+    plt.box(None)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    fig.colorbar(points, label="Cost/m2")
     return fig
 
-def price_Built_surface():
+def price_per_surface(nb_points=50000):
+    df = data[:nb_points]
     fig, ax = plt.subplots(figsize=(3.,2.3))
+    ax.scatter(df['Built surface'], df['Transaction cost']*1e-6)
+    ax.title.set_text("Paris real estate maps")
+    ax.set_xlabel("Surface (m2)")
+    ax.set_ylabel("Price (M€)")
+    z, formula = trend_line(df['Built surface'], df['Transaction cost'], "Surface", "Price")
+    plt.plot(df['Built surface'], z*1e-6,'--r')
+    plt.title(formula)
+    return fig
+
+def trend_line(x,y,x_name: str, y_name: str):
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    if round(p(0))>0:
+        formula = f"{y_name} = {round(p(1)-p(0))} * {x_name} + {round(p(0))}"
+    else:
+        formula = f"{y_name} = {round(p(1)-p(0))} * {x_name} - {round(-p(0))}"
+    return p(x), formula
     
