39280fa731347d50f3a0436dc7ea5d855d87248e
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-07 21:56:00 +0100

    [ADD] add delta days in the model

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from sklearn.ensemble import RandomForestRegressor 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import r2_score
 import pickle
 import numpy as np
 
@@ -11,11 +11,9 @@ def MAPE(true, pred):
     return (100*np.abs(true-pred)/true).mean()
 
 x_train, x_test, y_train, y_test = train_test_split(data[[
-    "Built surface","Number of rooms", "Longitude","Latitude", "District"]], 
+    "Built surface", "Number of rooms", "Longitude", "Latitude", "District", "Delta days"]], 
 data.iloc[:,0].values.ravel(), test_size=.2, random_state=1) # split train_set and test_set
 
-model = RandomForestRegressor(n_estimators=100, min_samples_split=4)
+model = RandomForestRegressor(n_estimators=200, min_samples_split=30)
 model.fit(x_train, y_train)
 pickle.dump(model, open('src/model.pkl', 'wb'))
-# preds = model.predict(x_test)
-# print(MAPE(y_test, preds), mean_squared_error(y_test, preds), r2_score(y_test, preds))
