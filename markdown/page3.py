290fdb6a88902f2756121c30668d151046f5fefa
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-07 21:51:34 +0100

    [ADD] price_per_room

--- markdown/page3.py
+++ markdown/page3.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from src.visualize import real_estate_map, price_per_surface, price_per_room, price_by_district
+from src.visualize import (real_estate_map, price_per_surface,
+ price_per_room, price_by_district, price_per_date)
 def page3():
     st.markdown("<h1>Data visualization</h1>",
      unsafe_allow_html=True)
@@ -8,7 +9,8 @@ def page3():
         "Surface": price_per_surface.price_per_surface,
         "Number of rooms": price_per_room.price_per_room,
         "Location": real_estate_map.real_estate_map,
-        "District": price_by_district.price_by_district
+        "District": price_by_district.price_by_district,
+        "Date": price_per_date.price_per_date
     }
 
     selected_visualization = st.selectbox("Choose a parameter:", parameter_to_visualized.keys())
--- src/visualize/price_per_room.py
+++ src/visualize/price_per_room.py
@@ -1,2 +1,13 @@
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+
+file = open("dset/data.pkl","rb")
+data =pickle.load(file)
+
 def price_per_room():
-    pass
+    fig, ax = plt.subplots(figsize=(3.,2.3))
+    avg_price = data.groupby('Number of rooms')['Cost per m2'].agg(np.mean)
+    district_ordered = list(set(data['Number of rooms']))
+    ax.bar(district_ordered,avg_price)
+    return fig
