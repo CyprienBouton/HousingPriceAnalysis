c11208848c2429dd8f1f31081614f960bd3b2a88
Cyprien Bouton <cyprienbouton@yahoo.fr>
2022-12-08 16:39:53 +0100

    [UP] markdown main_page.py

--- markdown/main_page.py
+++ markdown/main_page.py
@@ -1,5 +1,13 @@
 import streamlit as st
+from . import page2, page3, page4
+
 def main_page():
     st.markdown("<h1>Flat price prediction in Paris</h1>", unsafe_allow_html=True)
-    st.markdown("The purpose of this project is to predict the price of flat in Paris.")
-    st.markdown("",unsafe_allow_html=True)
+    st.markdown("""
+    The purpose of this project is to predict the price of flats in Paris and to visualize
+    the housing market in Paris.<br/><br/>
+    There are 3 sections in this app:
+    - Introduction
+    - Data visualization
+    - Flat price prediction
+    """, unsafe_allow_html=True)
