ddaac56526c208d0b8116ae55ecdcb8930831745
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-01-10 19:36:10 +0100

    [UP] model clean code

--- model.py
+++ model.py
@@ -4,16 +4,17 @@ from sklearn.metrics import r2_score
 import pickle
 import numpy as np
 
-file = open("dset/data.pkl","rb")
-data = pickle.load(file)
+if __name__=="__main__":
+    file = open("dset/data.pkl","rb")
+    data = pickle.load(file)
 
-def MAPE(true, pred):
-    return (100*np.abs(true-pred)/true).mean()
+    def MAPE(true, pred):
+        return (100*np.abs(true-pred)/true).mean()
 
-x_train, x_test, y_train, y_test = train_test_split(data[[
+    x_train, x_test, y_train, y_test = train_test_split(data[[
     "Built surface", "Number of rooms", "Longitude", "Latitude", "District", "Delta days"]], 
-data.iloc[:,0].values.ravel(), test_size=.2, random_state=1) # split train_set and test_set
+    data.iloc[:,0].values.ravel(), test_size=.2, random_state=1) # split train_set and test_set
 
-model = RandomForestRegressor(n_estimators=200, min_samples_split=30)
-model.fit(x_train, y_train)
-pickle.dump(model, open('src/model.pkl', 'wb'))
+    model = RandomForestRegressor(n_estimators=200, min_samples_split=30)
+    model.fit(x_train, y_train)
+    pickle.dump(model, open('src/model.pkl', 'wb'))
