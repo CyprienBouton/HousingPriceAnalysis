4ef405e8925a7c7659d8651eb8243eff086a571c
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-01-10 22:32:06 +0100

    [UP] streamlit_pages/pages4 add a warning

--- streamlit_pages/page4.py
+++ streamlit_pages/page4.py
@@ -8,14 +8,14 @@ def price_estimation(flat, file):
         preds = flat.predict_price(model)
         return st.write("We estimate that your flat worth "+str(preds)+" €")
     else:
-        return st.write("complete all the missing fields")    
+        return st.warning('You have to upload the model to estimate the price!', icon="⚠️")    
 
 def page4():
     st.markdown("<h1>Flat price prediction</h1>",
      unsafe_allow_html=True)
     surface = st.slider("Surface", min_value=9, max_value=300, value=50, step=None)
     nb_rooms = st.number_input("Number of rooms", min_value=1, max_value=50)
-    adress = st.text_input("adress")
+    adress = st.text_input("Address")
     district = st.selectbox("District:",[i for i in range(1,21)])
     date = st.date_input("Date of the transaction")
     yourFlat = Flat(surface, nb_rooms, adress, district, date)
