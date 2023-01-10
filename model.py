ae45c5df10327863a8cb2c907541d3a087da75bd
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-01-10 23:49:30 +0100

    [UP] model

--- model.py
+++ model.py
@@ -1,10 +1,6 @@
 from sklearn.ensemble import RandomForestRegressor 
 from sklearn.model_selection import train_test_split
 import pickle
-import numpy as np
-
-def MAPE(true, pred):
-    return (100*np.abs(true-pred)/true).mean()
 
 file = open("dset/data.pkl","rb")
 data = pickle.load(file)
@@ -13,10 +9,15 @@ def split_data(data):
     x_train, x_test, y_train, y_test = train_test_split(data[[
         "Built surface", "Number of rooms", "Longitude", "Latitude", "District", "Delta days"]], 
     data["Transaction cost"].values, test_size=.2, random_state=1) # split train_set and test_set
-    for dataset in [x_train, x_test, y_train, y_test]:
-        pickle.dump(dataset, open('src/' + dataset + '.pkl','wb'))
+    datasets = {
+        "x_train":x_train,
+        "x_test":x_test,
+        "y_train":y_train,
+        "y_test":y_test
+        }
+    for dataset in datasets:
+        pickle.dump(datasets[dataset], open('dset/' + dataset + '.pkl','wb'))
     return x_train, x_test, y_train, y_test
-
 
 if __name__=="__main__":
     model = RandomForestRegressor(n_estimators=200, min_samples_split=30)
