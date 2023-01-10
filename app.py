32e46b4548cd3d4af4b84f200d81290734b33556
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-01-10 19:37:47 +0100

    [UP] app clean code

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from streamlit_pages import main_page, page2, page3, page4
 
-page_names_to_funcs = {
+if __name__=="__main__":
+    page_names_to_funcs = {
     "General Description": main_page.main_page,
     "Dataset": page2.page2,
     "Data vizualisation": page3.page3,
     "Flat Price prediction": page4.page4
-}
+    }
 
-selected_page = st.sidebar.selectbox("Go to page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+    selected_page = st.sidebar.selectbox("Go to page", page_names_to_funcs.keys())
+    page_names_to_funcs[selected_page]()
