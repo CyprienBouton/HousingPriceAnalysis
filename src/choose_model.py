2a3215329a6298a7a6beb31424080a78a72e720b
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-01-12 16:33:38 +0100

    [FIX] choose_model upload custom model only if the user select it

--- src/choose_model.py
+++ src/choose_model.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pickle
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
@@ -6,16 +7,25 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
 from sklearn.gaussian_process import GaussianProcessRegressor
 
+def custom():
+    # Upload a custom model and then train it.
+    file = st.file_uploader("Import a model you'd like to train (.pkl format)",type='pkl')
+    return file
+
 def choose_model():
+    """"
+    Function allowing to build a model and then to train it.
+    """
     model_names = {
-        "Linear": LinearRegression(),
-        "Ridge": Ridge(),
-        "Lasso": Lasso(),
-        "DecisionTreeRegressor": DecisionTreeRegressor(),
-        "Random Forest": RandomForestRegressor(),
-        "KNN": KNeighborsRegressor(),
-        "Gaussian": GaussianProcessRegressor()
+        "Linear": LinearRegression,
+        "Ridge": Ridge,
+        "Lasso": Lasso,
+        "DecisionTreeRegressor": DecisionTreeRegressor,
+        "Random Forest": RandomForestRegressor,
+        "KNN": KNeighborsRegressor,
+        "Gaussian": GaussianProcessRegressor,
+        "Custom": custom
     }
     model_name = st.selectbox("Model", model_names.keys())
-    model = model_names[model_name]
+    model = model_names[model_name]()
     return model
