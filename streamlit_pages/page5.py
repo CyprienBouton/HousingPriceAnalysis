2250042c5b904f98212c000442ead956f8d504cc
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-01-12 16:35:04 +0100

    [UP] streamlit_pages/page5

--- streamlit_pages/page5.py
+++ streamlit_pages/page5.py
@@ -4,16 +4,20 @@ import numpy as np
 from src.choose_model import choose_model
 
 def MAPE(true, pred):
+    # return the mean average pourcentage error
     return (100*np.abs(true-pred)/true).mean()
     
 def page5():
+    #Page allowing to create a model.
     st.markdown("<h1>Create your own model</h1>", unsafe_allow_html=True)
     model = choose_model()
     x_train = pickle.load(open('dset/x_train.pkl','rb'))
     y_train = pickle.load(open('dset/y_train.pkl','rb'))
     x_test = pickle.load(open('dset/x_test.pkl','rb'))
     y_test = pickle.load(open('dset/y_test.pkl','rb'))
-    model.fit(x_train, y_train)
-    error = MAPE(y_test, model.predict(x_test))
-    st.write("""Your model has an error of {:.2f} %""".format(error))
-    st.download_button("Download Model", data=pickle.dumps(model), file_name="model.pkl")
+    if st.button("Fit"):
+        with st.spinner("Wait, your model is training"):
+            model.fit(x_train, y_train)
+        error = MAPE(y_test, model.predict(x_test))
+        st.write("""Your model has a mean error of {:.2f} %""".format(error))
+        st.download_button("Download model", data=pickle.dumps(model), file_name="model.pkl")
