4bf4ee6c4ef0585f9f5dee32b5e67dbdc6a45369
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-09-06 20:02:58 -0400

    [UP] streamlit_pages/page4

--- streamlit_pages/page4.py
+++ streamlit_pages/page4.py
@@ -5,8 +5,8 @@ import pickle
 def price_estimation(flat, file):
     #compute the price if the model is loaded otherwise return a warning
     if file!=None: # first load the model
-        model = pickle.load(file)
-        preds = flat.predict_price(model)
+        models_dict = pickle.load(file)
+        preds = flat.predict_price(models_dict)
         return st.write("We estimate that your flat worth "+str(preds)+" €")
     else:
         return st.warning('You have to upload a model to estimate the price!', icon="⚠️")    
@@ -23,4 +23,4 @@ def page4():
     file = st.file_uploader("Import your model (.pkl format)",type='pkl')
     prediction = st.button("Price estimation")
     if prediction:
-        price_estimation(yourFlat,file)
+        price_estimation(yourFlat, file)
